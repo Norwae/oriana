9e3efc573a1b9bd4840b7ef26a5e733bac467b84
Terra <terra@spacetime.de>
2016-02-10 11:18:41 +0100

    INITIAL: Convenience for transactions

--- src/main/scala/slikka/package.scala
+++ src/main/scala/slikka/package.scala
@@ -7,7 +7,11 @@ import scala.concurrent.{ExecutionContext, Future}
 package object slikka {
   type DBOperation[-Context <: DatabaseContext, +Result] = (Context) => Future[Result]
 
-  def runInDBContext[T: Manifest](op: DBOperation[_ <: DatabaseContext, T], actorName: String = "database")(implicit actorRefFactory: ActorRefFactory, timeout: Timeout, ec: ExecutionContext): Future[T] = {
+  def executeDBOperation[T: Manifest](op: DBOperation[_ <: DatabaseContext, T], actorName: String = "database")(implicit actorRefFactory: ActorRefFactory, timeout: Timeout, ec: ExecutionContext): Future[T] = {
+    (actorRefFactory.actorSelection(s"/user/$actorName") ? op).mapTo[T]
+  }
+
+  def executeDBTransaction[T: Manifest](op: DBTransaction[_ <: DatabaseContext, T, _, _], actorName: String = "database")(implicit actorRefFactory: ActorRefFactory, timeout: Timeout, ec: ExecutionContext): Future[T] = {
     (actorRefFactory.actorSelection(s"/user/$actorName") ? op).mapTo[T]
   }
 }
