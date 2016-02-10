4816ae4216bc3803e3e0543dab0141d04d25dbe5
Terra <terra@spacetime.de>
2016-02-10 09:37:54 +0100

    INITIAL: Never retry the init block

--- src/main/scala/slikka/DBInitializer.scala
+++ src/main/scala/slikka/DBInitializer.scala
@@ -1,6 +1,5 @@
 package slikka
 
 trait DBInitializer extends DBOperation[DatabaseContext, DatabaseActor.InitComplete.type]{
-  def allowStartupOnFailure = false
-  override def retrySchedule = None
+  final override def retrySchedule = Some(NoRetrySchedule)
 }
