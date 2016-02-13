0de9bb1f4491c0e437716a475703b132f5a95bd1
Terra <terra@spacetime.de>
2016-02-13 22:02:45 +0100

    INITIAL: Finish doc

--- src/main/scala/oriana/DatabaseActor.scala
+++ src/main/scala/oriana/DatabaseActor.scala
@@ -7,21 +7,41 @@ import slick.dbio.{Effect, NoStream}
 
 import scala.collection.mutable
 
+/**
+  * The central point of oriana, an actor that enables an init lifecycle for a database, and
+  * a parent to individual operation actors.
+  *
+  * This actor goes through 3 lifecvcle stages. Uninitialized, where it accepts simple operations and transactions
+  * (operations for short), changes to its retry configuration and changes to the initializer. It will queue,
+  * but not execute any operations it receives. These queued operations will use the retry configuration that
+  * was active when they were first received. Once it received the [DatabaseActor.Init] message, it will
+  * evolve to the initializing state.
+  *
+  * While the actor is initializing, it executes the configured initializer. It will accept operations and keep them
+  * queued, and accept changes to the retry configuration. If initialization succeeds, it will enter the ready state.
+  * If initialization fails, the failure will trigger an exception on this actor, which will (in turn) lead to
+  * the actors supervisor deciding the further fate of the database access. Upon entering the ready state, all
+  * previously queued operations are fired
+  *
+  * In the ready state, the actor accepts retry policy changes and operations, which start executing immediately
+  *
+  * @param dbAccess database connection
+  */
 class DatabaseActor(dbAccess: ExecutableDatabaseContext) extends Actor {
   import DatabaseActor._
-  val log = LoggerFactory.getLogger(classOf[DatabaseActor])
+  private val log = LoggerFactory.getLogger(classOf[DatabaseActor])
 
-  var schedule: RetrySchedule = DefaultSchedule
-  var init: DBInitializer = SchemaCreateInitializer
-  val waiting = mutable.Buffer[ActorRef]()
+  private var schedule: RetrySchedule = DefaultSchedule
+  private var init: DBInitializer = SchemaCreateInitializer
+  private val waiting = mutable.Buffer[ActorRef]()
 
-  val changeSchedule: Receive = {
+  private val changeSchedule: Receive = {
     case schedule: RetrySchedule =>
       this.schedule = schedule
       log.info("Installed new default retry schedule")
   }
 
-  val bufferOperation: Receive = {
+  private val bufferOperation: Receive = {
     case op: DBOperation[_, _] =>
       waiting += prepareOperation(op, NoRetrySchedule, sender())
       log.debug("queued a non-transactional operation")
@@ -30,12 +50,12 @@ class DatabaseActor(dbAccess: ExecutableDatabaseContext) extends Actor {
       log.debug("queued a transactional operation")
   }
 
-  val ready: Receive = changeSchedule orElse {
+  private val ready: Receive = changeSchedule orElse {
     case op: DBOperation[_, _] => prepareOperation(op, NoRetrySchedule, sender()) ! Start(dbAccess)
     case tr: DBTransaction[_, Any, NoStream, Effect] => prepareTransaction(tr) ! Start(dbAccess)
   }
 
-  val initializing: Receive = changeSchedule orElse bufferOperation orElse {
+  private val initializing: Receive = changeSchedule orElse bufferOperation orElse {
     case InitComplete =>
       log.info(s"Init complete, now starting ${waiting.size} waiting tasks")
       waiting.foreach(_ ! Start(dbAccess))
@@ -46,7 +66,7 @@ class DatabaseActor(dbAccess: ExecutableDatabaseContext) extends Actor {
       throw e
   }
 
-  val beforeInit: Receive = changeSchedule orElse ({
+  private val beforeInit: Receive = changeSchedule orElse ({
     case init: DBInitializer =>
       this.init = init
       log.info(s"Set database initializer to an ${init.getClass.getName}")
